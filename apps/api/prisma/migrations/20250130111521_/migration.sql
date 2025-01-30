1b7e9c773ac92f80a79eeadf40397d18741d9078
sixtycode <sagaraperkasa.work@gmail.com>
2025-01-30 18:16:41 +0700

    refactor: prisma sample migration

--- apps/api/prisma/migrations/20250130111521_/migration.sql
+++ apps/api/prisma/migrations/20250130111521_/migration.sql
@@ -278,6 +278,18 @@ CREATE TABLE `PaymentMethod` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `Sample` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `code` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Sample_code_key`(`code`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Admins` ADD CONSTRAINT `Admins_role_id_fkey` FOREIGN KEY (`role_id`) REFERENCES `Roles`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
